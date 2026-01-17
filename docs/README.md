f03a81baef517aa3a5f10d29951625c6907d3b91
ZesterJ <yjianwei.05@gmail.com>
2026-01-17 20:52:07 +0800

    Level -0
    Implementation of Level 0.
    Rename, Greet, Exit.

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Eve User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Eve.java
@@ -0,0 +1,16 @@
+public class Eve {
+    public static void main(String[] args) {
+        String logo =  " _____             _____ \n"
+                    + "| ____|  \\     /  | ____|\n"
+                    + "|  _|     \\   /   |  _|  \n"
+                    + "| |___     \\ /    | |___ \n"
+                    + "|_____|     V     |_____|\n";
+
+        System.out.println("Hello! I am\n" + logo);
+        System.out.println("How may I be of assistance today?");
+        System.out.println("__________________________________\n");
+
+        System.out.println("Goodbye. Have a nice day!");
+        System.out.println("__________________________________");
+}
+}
