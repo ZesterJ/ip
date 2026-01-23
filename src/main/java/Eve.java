e43fc051352daa45a28dbcff9d22601f4811594e
ZesterJ <yjianwei.05@gmail.com>
2026-01-23 17:48:01 +0800

    Level-2
    Level 2. Add, List
    
    Added the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Eve.java
+++ src/main/java/Eve.java
@@ -13,7 +13,8 @@ public class Eve {
         System.out.println("How may I be of assistance today?");
         System.out.println("__________________________________\n");
 
-
+        String[] tasks = new String[100];
+        int taskCount = 0;
         Scanner sc = new Scanner(System.in);
         
         while (true) {
@@ -26,9 +27,22 @@ public class Eve {
                 System.out.println("__________________________________\n");
                 sc.close();
                 return;
+            case "list":
+                if (taskCount == 0){
+                  System.out.println("Your Task List is empty.");
+                  break;  
+                } else {
+                    for(int i = 0; i < taskCount; i += 1){
+                        System.out.println((i+1) + ". " + tasks[i]);
+                    }
+                    System.out.println("__________________________________\n");
+                }
+                break;
             default:
-                System.out.println(userInput);
+                System.out.println("added: " + userInput);
                 System.out.println("__________________________________\n");
+                tasks[taskCount] = userInput;
+                taskCount += 1;
                 break;
             }
         }
